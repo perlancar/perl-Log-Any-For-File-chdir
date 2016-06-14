39ef178c96a1e62db05173bfc8ec34c0d90b48de
perlancar (@pc-office) <perlancar@gmail.com>
2016-06-14 18:54:16 +0700

    Initial commit

---
+++ lib/Log/Any/For/File/chdir.pm
@@ -0,0 +1,46 @@
+package Log::Any::For::File::chdir;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::Any '$log';
+
+use File::chdir ();
+use Monkey::Patch::Action qw(patch_package);
+
+our $h = patch_package(
+    'File::chdir::SCALAR',
+    '_chdir',
+    'wrap',
+    sub {
+        my $ctx = shift;
+        $log->tracef("[File::chdir] chdir(%s)", $_[0]);
+        $ctx->{orig}->(@_);
+    },
+);
+
+1;
+# ABSTRACT: Add logging to File::chdir
+
+=head1 SYNOPSIS
+
+ use Log::Any::For::File::chdir;
+ use File::chdir;
+
+ $CWD = "foo";
+ ...
+ $CWD = "bar";
+ ...
+
+will produce these log statements (at the trace level):
+
+ [File::chdir] chdir(foo)
+ [File::chdir] chdir(bar)
+
+
+=head1 SEE ALSO
+
+L<File::chdir>
